9d79a1c661e5e7441b69c0146beb19c6fbf08099
Ankit Jangir <ankit2000jangir@gmail.com>
2020-07-04 23:33:25 +0530

    added static path folders

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
-
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 
+// Set static folders
+app.use(express.static(path.join(__dirname, "public")));
 
+// Run when client connect
+io.on("connection", socket => {
+  console.log("New WS Connection...");
+});
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -13,4 +22,4 @@ app.get('/', (req, res) => res.send('Hello World!'))
 const PORT = 3000 || process.env.PORT;
 
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
